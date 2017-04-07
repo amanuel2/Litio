0262d71938196e0fa0c05322c0ae10fab459626d
Amanuel <abogale2@gmail.c>
2017-04-07 14:32:01 +0000

    Commit

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
 var express = require('express');
 var path    = require('path');
+var bodyParser = require("body-parser");
+var mongoose = require('mongoose');
+var regCtrl = require('./controllers/registerController.js');
 
 var app = express();
 const PORT = 8080;
 
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.use(express.static(__dirname + '/assets'))
 
-
-var root_dir_opts = {
-  root :  __dirname + "/",
-  dotfiles: 'deny',
-  headers: {
-      'x-timestamp': Date.now(),
-      'x-sent': true
-  }
-}
+var dir_opts_public =  {
+    root :  __dirname + "/public",
+    dotfiles: 'deny',
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    }
+}  
+  
+regCtrl(app,urlencodedParser,dir_opts_public);
 
 app.get('/', function(req,res){
-  res.sendFile("index.html",root_dir_opts, function(err){
-    if(err) {
-      console.log("ERROR : " + err);
+  res.sendFile("index.html",dir_opts_public,function(err){
+    if(err) 
       throw err;
-    }
     else
       console.log(req.get("host"));
   });
-})
+});
+
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
   console.log("LISTENING AT PORT AND IP" + process.env.PORT, process.env.IP);
